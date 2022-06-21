bfa6131b9c45dd94368ba1382daf4b9fa62bc2dc
rubacodes <rubacodes@gmail.com>
2022-06-21 15:36:55 +0200

    get corretto della badword

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
     //testo di partenza e sua lunghezza
     $text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do dolor tempor incididunt ut labore et dolore magna aliqua. Ut dolor ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut dolor ex ea commodo consequat. Duis aute irure dolor in reprehenderit in dolor velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     $textLength = strlen($text);
-    //badwordws
-    $_GET = ["badword"];
-    $censoredText = str_replace("dolor", "***", $text);
+    //badword
+    //$_GET = ["badword"];
+    var_dump($_GET);
+    $censoredText = str_replace( $_GET = ["badword"], "***", $text);
 ?>
 
 <!DOCTYPE html>
@@ -15,21 +16,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP BAD WORDS</title>
 </head>
-
-<!-- <form action="post">
-    <input type="text" name="badword">
-    <button type="submit">submit</button>
-</form> -->
- <h1>testo</h1>
- <div>
-    <p> <?php echo $text ?></p>
-    <span> Il testo e' lungo: <?php echo $textLength?> caratteri</span>
- </div>
-  <div>
-    <p> <?php echo $censoredText ?></p>
- </div>
-
 <body>
-    
+    <!-- <form action="get">
+        <input type="text" name="badword">
+        <button type="submit">submit</button>
+    </form> -->
+    <h1>testo</h1>
+    <div>
+        <p> <?php echo $text ?></p>
+        <span> Il testo e' lungo: <?php echo $textLength?> caratteri</span>
+    </div>
+    <div>
+        <p> <?php echo $censoredText ?></p>
+    </div>  
 </body>
 </html>
